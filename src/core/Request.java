d2c0fad775af4ccd9d8025707f1878fe0792818e
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-04-30 22:10:49 -0700

    Starting up the Request class

---
+++ src/core/Request.java
@@ -0,0 +1,17 @@
+package core;
+
+// Data streaming
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+//
+
+public class Request {
+    public Request(String url) {
+
+    }
+
+    public Request(String url, String method) {
+
+    }
+}
