7cbfbbfeb053c7ab83f147a832921c8596b15603
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-04-30 22:26:52 -0700

    Added request constants

---
+++ src/core/constants/Constants.java
@@ -0,0 +1,10 @@
+package core.constants;
+
+public class Constants {
+    public static String
+            GET = "GET",
+            POST = "POST",
+            DELETE = "DELETE",
+            PUT = "PUT",
+            PATCH = "PATCH";
+}
