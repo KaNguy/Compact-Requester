46e84b8e72187931f1976b8b8bc0b76a769ed596
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-05-01 11:26:16 -0700

    Working on the headers support, still needs to be fixed since there is a "java.lang.IllegalStateException: Already connected" error which is affected by setting the headers.

--- src/core/Request.java
+++ src/core/Request.java
@@ -25,9 +25,20 @@ import core.constants.Constants;
 public class Request {
     public static void main(String[] args) {
         Request request1 = new Request("https://docs.scala-lang.org/", "GET", null);
-        Request request2 = new Request("https://reqres.in/api/users", "POST", "{\"name\": \"Li Xi\", \"job\": \"Java POST\"}");
         System.out.println(request1.output);
+
+        Request request2 = new Request("https://reqres.in/api/users", "POST", "{\"name\": \"Li Xi\", \"job\": \"Java POST\"}");
         System.out.println(request2.output);
+
+        Request request3 = new Request(
+                "https://api.github.com/",
+                "GET",
+                null,
+                new String[][] {
+                    {"Accept", "*/*"},
+                    {"User-Agent", "*"}
+                });
+        System.out.println(request3.output);
     }
     public String method;
     public String url;
